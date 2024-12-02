fd9103dc7f22ceed34af6bfefc290ddac6185edb
Priyash Patil <38959321+priyashpatil@users.noreply.github.com>
2024-12-02 17:59:11 +0530

    add package card component

---
+++ resources/views/components/package-card.blade.php
@@ -0,0 +1,16 @@
+<div class="card shadow-sm">
+    <img src="{{ asset('images/card-img.png') }}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title text-success mb-0">Everest Trek</h5>
+        <div class="mb-2">Kathmandu - 4 Days Tour</div>
+        <div class="d-flex justify-content-between mb-2">
+            <div>14 Days</div>
+            <div><del>$1450</del>
+                <span class="text-success fw-bold">$1440</span>
+            </div>
+        </div>
+        <p class="card-text">Embracing the journey to Everest, where every step unveils breathtaking views and
+            unforgettable memories. Adventure awaits!</p>
+        <a href="#" class="btn btn-primary">More Details</a>
+    </div>
+</div>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -33,7 +33,7 @@
         <h2 class="text-success">The best place you must visit</h2>
         <p class="mb-5">A wonderful serenity to have in ur bucket list</p>
 
-        <div class="splide" aria-label="Popular destination">
+        <div id="popularDestination" class="splide" aria-label="Popular destination">
             <div class="splide__track">
                 <ul class="splide__list">
                     <li class="splide__slide p-4">
@@ -41,7 +41,6 @@
                             <h3>Popular destination</h3>
                             <p>A wonderful serenity has</p>
                         </div>
-                        {{-- <x-slide-one-card /> --}}
                     </li>
                     @for ($i = 0; $i < 10; $i++)
                         <li class="splide__slide">
@@ -53,17 +52,112 @@
         </div>
     </section>
 
+    <section class="bg-light py-5">
+        <div class="container py-5 my-5">
+
+            <div class="shadow p-3">
+                <h2 class="text-success">WHY CHOOSE US ?</h2>
+                <p>We believe that traveling and touring is much more than just a vacation but rather an opportunity to get
+                    one
+                <p>We believe that traveling and touring is much more than just a vacation but rather an opportunity to get
+                    one
+                    with
+                    nature and enjoy the deeper aspect of its beauty. And, we work accordingly to provide our customers with
+                    similar
+                    experiences while on their treks and tours. On our treks and expeditions, we incorporate different types
+                    of
+                    experiences while on their treks and tours. On our treks and expeditions, we incorporate different types
+                    of
+                    experiences to enjoy nature, get to know the locals, and understand the culture while having fun. We
+                    prioritize
+                    blending in with the locals during the trek for cultural and linguistic experience, traditional
+                    exposure,
+                    blending in with the locals during the trek for cultural and linguistic experience, traditional
+                    exposure,
+                    and
+                    understanding of the place.</p>
+
+                <a href="#" class="btn btn-primary">Explore</a>
+            </div>
+
+        </div>
+    </section>
+
+    <section class="py-5 my-5">
+        <div class="container py-5">
+
+            <div class="shadow p-3">
+                <h2 class="text-success">Our Achievement In Number</h2>
+                <p>Join countless happy trekkers on breathtaking eco-friendly adventures!</p>
+
+                <div class="row">
+                    <div class="col-md-3">
+                        <div>354</div>
+                        <div>Destination</div>
+                    </div>
+                    <div class="col-md-3">
+                        <div>354</div>
+                        <div>Destination</div>
+                    </div>
+                    <div class="col-md-3">
+                        <div>354</div>
+                        <div>Destination</div>
+                    </div>
+                    <div class="col-md-3">
+                        <div>354</div>
+                        <div>Destination</div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+
+    <section class="bg-light py-5">
+        <div class="container py-5 my-5">
+            <h2 class="text-success">Most Popular Packages</h2>
+
+            <div id="popularPackages" class="splide" aria-label="Most Popular Packages">
+                <div class="splide__track">
+                    <ul class="splide__list">
+                        @for ($i = 0; $i < 5; $i++)
+                            <li class="splide__slide">
+                                <x-package-card />
+                            </li>
+                        @endfor
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
     <script>
         document.addEventListener('DOMContentLoaded', function() {
-            var splide = new Splide('.splide', {
+            var popularDestination = new Splide('#popularDestination', {
                 type: 'loop',
-                // perPage: 5,
                 perMove: 1,
                 autoWidth: true,
                 gap: '1rem',
                 pagination: false,
             });
-            splide.mount();
+            popularDestination.mount();
+
+            var popularPackages = new Splide('#popularPackages', {
+                type: 'loop',
+                perPage: 3,
+                perMove: 1,
+                gap: '2rem',
+                pagination: false,
+                breakpoints: {
+                    992: {
+                        perPage: 2,
+                    },
+                    768: {
+                        perPage: 1,
+                    }
+                }
+            });
+            popularPackages.mount();
         });
     </script>
 @endsection
