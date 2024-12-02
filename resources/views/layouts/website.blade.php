7f701a0641b4a958f889d39d80b374e6e667e0aa
Priyash Patil <38959321+priyashpatil@users.noreply.github.com>
2024-12-02 11:28:09 +0530

    add navbar content

--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -10,43 +10,94 @@
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg bg-body-tertiary">
-        <div class="container">
-            <a class="navbar-brand" href="{{ route('home') }}">Hiking Nepal</a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('home') }}">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('deals') }}">Deals</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
-                            About
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="{{ route('information') }}">Information</a></li>
-                            <li><a class="dropdown-item" href="{{ route('who-we-are') }}">Who We Are</a></li>
-                            <li><a class="dropdown-item" href="{{ route('what-we-offer') }}">What We Offer</a></li>
-                            <li><a class="dropdown-item" href="{{ route('booking-terms') }}">Booking Terms</a></li>
-                            <li><a class="dropdown-item" href="{{ route('legal-document') }}">Legal Document</a></li>
-                            <li><a class="dropdown-item" href="{{ route('our-team') }}">Our Team</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('contact') }}">Contact</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('blog') }}">Blog</a>
-                    </li>
-                </ul>
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="d-block w-100">
+            <div class="container d-flex align-items-center">
+                <a class="navbar-brand" href="{{ route('home') }}">
+                    <img src="{{ asset('logo.png') }}" alt="hiking nepal logo" width="208" height="auto">
+                </a>
+
                 <div class="ms-auto">
+                    <span class="me-3 d-inline-flex align-items-center gap-1">Join Groups: <img
+                            src="{{ asset('ic_outline-whatsapp.png') }}" alt="whatsapp" width="24" height="24">
+                        +977-9802342080</span>
+
+                    <a href="{{ route('deals') }}" class="btn btn-primary me-3">Deals</a>
                     <a href="{{ route('book-trip') }}" class="btn btn-primary">Pay Online</a>
+
+                    <button class="navbar-toggler bg-primary" type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
+                        aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                </div>
+            </div>
+
+            <div class="collapse navbar-collapse bg-primary w-100" id="navbarNav">
+                <div class="container">
+                    <ul class="navbar-nav">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('home') }}">Home</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                                Nepal
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Link 1</a></li>
+                                <li><a class="dropdown-item" href="#">Link 1</a></li>
+                                <li><a class="dropdown-item" href="#">Link 1</a></li>
+                                <li><a class="dropdown-item" href="#">Link 1</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                                India
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Link 1</a></li>
+                                <li><a class="dropdown-item" href="#">Link 1</a></li>
+                                <li><a class="dropdown-item" href="#">Link 1</a></li>
+                                <li><a class="dropdown-item" href="#">Link 1</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                                Bhutan
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="#">Link 1</a></li>
+                                <li><a class="dropdown-item" href="#">Link 1</a></li>
+                                <li><a class="dropdown-item" href="#">Link 1</a></li>
+                                <li><a class="dropdown-item" href="#">Link 1</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('information') }}">Information</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                                About
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="{{ route('who-we-are') }}">Who We Are</a></li>
+                                <li><a class="dropdown-item" href="{{ route('what-we-offer') }}">What We Offer</a></li>
+                                <li><a class="dropdown-item" href="{{ route('booking-terms') }}">Booking Terms</a></li>
+                                <li><a class="dropdown-item" href="{{ route('legal-document') }}">Legal Document</a>
+                                </li>
+                                <li><a class="dropdown-item" href="{{ route('our-team') }}">Our Team</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('blog') }}">Blog</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('contact') }}">Contact</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('deals') }}">Deals</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </div>
