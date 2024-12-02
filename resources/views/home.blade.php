6200c26b25c7bdd6b58c743a9b947a9ee7005918
Priyash Patil <38959321+priyashpatil@users.noreply.github.com>
2024-12-02 14:57:17 +0530

    add hero animation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,9 @@
 @section('content')
     <section class="hero">
         <div class="container">
-            <div class="hero-cloud-r1"></div>
-            <h1 class="">Hiking Nepal</h1>
-            <form action="" method="GET">
+            <div class="hero-cloud-r1 animate__animated animate__slideInRight"></div>
+            <h1 class="animate__animated animate__slideInUp">Hiking Nepal</h1>
+            <form action="" method="GET" class="animate__animated animate__zoomIn">
                 <div class="input-group mb-3">
                     <span class="input-group-text" id="basic-addon1">
                         <img src="{{ asset('images/search.png') }}" alt="search" width="18" height="18">
@@ -16,15 +16,16 @@
                 </div>
             </form>
 
-            <img src="/images/mount-img-center-top.png" class="hero-mount-center-top" alt="">
-            <img src="/images/mount-img-l1.png" class="hero-mount-l1" alt="">
-            <img src="/images/mount-img-r1.png" class="hero-mount-r1" alt="">
-            <img src="/images/mount-img-center.png" class="hero-mount-center" alt="">
+            <img src="/images/mount-img-center-top.png" class="hero-mount-center-top animate__animated animate__slideInUp"
+                alt="">
+            <img src="/images/mount-img-l1.png" class="hero-mount-l1 animate__animated animate__slideInUp" alt="">
+            <img src="/images/mount-img-r1.png" class="hero-mount-r1 animate__animated animate__slideInUp" alt="">
+            <img src="/images/mount-img-center.png" class="hero-mount-center animate__animated animate__slideInUp"
+                alt="">
 
-            <div class="hero-cloud-l1"></div>
-            <div class="hero-cloud-l2"></div>
-
-            <div class="hero-cloud-center"></div>
+            <div class="hero-cloud-l1 animate__animated animate__slideInLeft"></div>
+            <div class="hero-cloud-l2 animate__animated animate__slideInLeft"></div>
+            <div class="hero-cloud-center animate__animated animate__slideInRight"></div>
         </div>
     </section>
 @endsection
