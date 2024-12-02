12681f0865e3f6615fa5d5cf92d747cbd64f8655
Priyash Patil <38959321+priyashpatil@users.noreply.github.com>
2024-12-02 10:52:54 +0530

    add links on layout

--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -10,6 +10,48 @@
 </head>
 
 <body>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container">
+            <a class="navbar-brand" href="{{ route('home') }}">Hiking Nepal</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('home') }}">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('deals') }}">Deals</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown">
+                            About
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="{{ route('information') }}">Information</a></li>
+                            <li><a class="dropdown-item" href="{{ route('who-we-are') }}">Who We Are</a></li>
+                            <li><a class="dropdown-item" href="{{ route('what-we-offer') }}">What We Offer</a></li>
+                            <li><a class="dropdown-item" href="{{ route('booking-terms') }}">Booking Terms</a></li>
+                            <li><a class="dropdown-item" href="{{ route('legal-document') }}">Legal Document</a></li>
+                            <li><a class="dropdown-item" href="{{ route('our-team') }}">Our Team</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('contact') }}">Contact</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('blog') }}">Blog</a>
+                    </li>
+                </ul>
+                <div class="ms-auto">
+                    <a href="{{ route('book-trip') }}" class="btn btn-primary">Pay Online</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+
     @yield('content')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
