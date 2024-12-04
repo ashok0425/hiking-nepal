39381f746601dbad35948a52977d01b074de970c
Priyash Patil <38959321+priyashpatil@users.noreply.github.com>
2024-12-04 21:05:59 +0530

    add discover section css

---
+++ resources/views/home/inc/discover.blade.php
@@ -0,0 +1,22 @@
+<section class="container my-5 py-5">
+    <div class="row g-3">
+        <div class="col-md-4 text-center">
+            <img src="{{ asset('images/reach.png') }}" alt="reach" width="125" height="115" class="mb-3">
+            <h3 class="text-success mb-3">Discover destinations beyond your reach</h3>
+            <p>Our journeys are guided by certified experts, offering you unique experiences in locations that remain
+                hidden to most.</p>
+        </div>
+        <div class="col-md-4 text-center">
+            <img src="{{ asset('images/earth.png') }}" alt="earth" width="125" height="115" class="mb-3">
+            <h3 class="text-success mb-3">Discover destinations beyond your reach</h3>
+            <p>Our journeys are guided by certified experts, offering you unique experiences in locations that remain
+                hidden to most.</p>
+        </div>
+        <div class="col-md-4 text-center">
+            <img src="{{ asset('images/fist.png') }}" alt="fist" width="125" height="115" class="mb-3">
+            <h3 class="text-success mb-3">Discover destinations beyond your reach</h3>
+            <p>Our journeys are guided by certified experts, offering you unique experiences in locations that remain
+                hidden to most.</p>
+        </div>
+    </div>
+</section>
