de25d1ab3553ee3a9f48e6c75a41e1a9349cf639
Priyash Patil <38959321+priyashpatil@users.noreply.github.com>
2024-12-04 21:54:32 +0530

    add information page content

--- resources/views/information.blade.php
+++ resources/views/information.blade.php
@@ -3,7 +3,155 @@
 @section('title', 'Hiking Nepal')
 
 @section('content')
-    <div class="container">
-        <h1>Information</h1>
+    <div class="position-relative overflow-hidden d-flex justify-content-center align-items-center" style="height: 330px;">
+        <img src="{{ asset('images/head-cover.jpeg') }}" alt="head cover" class="w-100 position-absolute start-0 top-0"
+            style="height: 330px; object-fit:cover; filter: brightness(80%) contrast(110%);">
+        <div class="container">
+            <h1 class="mb-0 z-1 position-relative text-uppercase text-white text-center">Information</h1>
+        </div>
     </div>
+
+    <section class="container py-md-5 my-5">
+        <h2 class="text-center text-success mb-4">Useful Information for the package</h2>
+        <p>At Hiking Nepal, our team is made up of passionate, experienced, and dedicated professionals who are committed to
+            providing unforgettable trekking experiences in the Himalayas. From seasoned guides with extensive knowledge of
+            Nepal's trekking routes to support staff who ensure your journey is smooth and comfortable, we work together to
+            make every adventure a safe and memorable one. Our team's deep love for the mountains, combined with their
+            expertise, ensures that you not only explore the breathtaking landscapes of Nepal but also immerse yourself in
+            its rich culture and heritage.</p>
+
+        <h2 class="text-success">Passport And Visa Requirements</h2>
+        <p>All foreigners require a visa for entry into Nepal (except Indian nationals). It is your responsibility to obtain
+            an entry visa. You can get it from a Nepalese embassy overseas or on arrival in Kathmandu, Nepal. Most people
+            will obtain their visa on arrival at Kathmandu airport. You will need two passport photos and the following fees
+            in US Dollar cash (this is payable in any major currency but NOT traveler's cheque) dependent upon the duration
+            of the multiple entry visas:</p>
+
+        <ol class="fw-bold">
+            <li>Multiple entries 15 days US$ 30 or equivalent convertible currency</li>
+            <li>Multiple entries 30 days US$ 50 or equivalent convertible currency</li>
+            <li>Multiple entries 90 days US$ 125 or equivalent convertible currency</li>
+        </ol>
+
+
+        <h2 class="text-success">Travel Insurance</h2>
+        <p>Travel Insurance is mandatory for booking any of Himalayas Nepal Visionâs Adventure packages. Trekking in the
+            Himalayas is full of fun and adventure but you have to be prepared for risk management. While you are in High
+            Altitude region, there are higher risks of difficulties. You may suffer from various health problems, injuries,
+            and illnesses. Even High altitude sickness can tear up your perfect itinerary. And thatâs where travel
+            insurance can save you from distress or at least from some of the costs, depending on the policy you buy. For
+            your adventure, Travel insurance is a safety net if things go wrong. At times, even a helicopter may be needed
+            along with a rescue team, for ground evacuation, so, every traveler must have rescue insurance covered against
+            personal accidents, medical expenses, emergency air ambulance/helicopter rescue service accidents, and personal
+            liability. Start your trip with all the necessary health & traveling insurance for your safety.
+            Having good travel insurance will not only guarantee you peace of mind while you travel with us but also provide
+            you with complete cover for anything things that may go wrong.</p>
+
+        <h2 class="text-success">Trip Meal Plan</h2>
+        <p>Eating is a big part of traveling. With Nepal Vision, you will experience the vast array of wonderful food that
+            is available in the Mountains. Nepal Vision will provide Traveling Mountains meals (breakfast, lunch & dinner)
+            that crews require on the trail. On this trip, you can enjoy both traditional Nepalese three-time served as Dal
+            Bhat (rice and lentils), as well as a variety of different food items, such as vegetables, noodles, potatoes
+            cuisine served and soup along with some, Nepali versions of western food such as pizza, pasta, and French fries.
+            Breakfast and dinner will be served from the teahouse or lodge menu where we will spend the night. Lunch will be
+            eaten at one of the trailside restaurants. A welcome dinner, a farewell dinner, and breakfasts will be served
+            during our stay in Kathmandu.
+            When an individual is allergic to some food products, requires a special diet, or has specific religious food
+            needs, we will arrange it as per your requirements on request. Also, the crew will carry fresh, fruit supplies
+            at a time along the trail. Our guides will carefully be checked for the quality and quantity of the food in
+            every teahouse. Explicit cooking directions are indicated on each food for your convenience.</p>
+
+        <h2 class="text-success">Trip Accommodation</h2>
+        <p>During this trip, we will spend a few nights in Kathmandu at four-star standard hotels. We will spend the nights
+            at standard mountain teahouses. A Tea House is a combination of a guesthouse, restaurant, and social hang-out.
+            Private rooms are available in most tea houses. The lodges are fairly basic. The rooms are spare with twin beds
+            and very little additional furniture. Blankets are generally provided. Most bathrooms are shared and toilets are
+            the western version. Most of the Teahouses have running water facilities while some of them may also have hot
+            water available for bathing at an additional cost. It is advisable to carry wet wipes as an alternative.
+            There is a large dining room more like the lounge, warmed by an iron cylinder, fitted with a chimney duct, in
+            which a log fire is lit. Most teahouses now also have electricity for charging small appliances, mobile phones,
+            and cameras. Some might charge a small amount for this.</p>
+
+        <h2 class="text-success">Best time to visit Nepal</h2>
+        <p>Nepal is for all seasons! But because of the huge range in altitude and landscape, the climate of Nepal differs
+            significantly throughout the country. The most recommended seasons for trekking are autumn (Sept, Oct, Nov) and
+            spring (March, April, and May). In these seasons you will be rewarded with good weather, sunny and warm with
+            clear skies and outstanding views. During monsoons (June, July, Aug) although there will be no problem with
+            trekking, the issue could be less visibility and rain. For the student of botanicals, a monsoon is a blessing as
+            the higher valleys, mountains, and meadows blossom with flowers and abundant vegetation. You can also trek in
+            winter (Dec, Jan, and Feb). Only, the issue will be cold weather with snowfall at higher elevations.
+            The trekking routes are filled with trekkers during spring and autumn but during monsoon and winter the routes
+            are not packed and you could enjoy rather the best of nature. However, because of the effects of global warming,
+            the climate is changing worldwide and there could always be the possibility of exceptions in these weather
+            patterns. There could be a hoax of bad weather and sometimes even in adverse months during the best seasons
+            too.</p>
+
+        <h2 class="text-success">Leader(s) and Staff</h2>
+        <p>The Trek Leader of your group has a role to ensure all aspects of the trip run smoothly. Our guides are
+            intimately familiar with all local castes and customs along the trails and will share with you many cultural
+            insights that are not covered in any guidebook. You will have a good chance to learn Nepali phrases, cultural
+            things, and more on your trekking. Nepal Vision trained its guide/trekking staff on the following course:</p>
+        <ol class="fw-bold">
+            <li>Intensive Wilderness First Aid</li>
+            <li>Trekking Guide Training</li>
+            <li>Trekking Workshop and Adventure Meet</li>
+            <li>Eco Trekking Workshop and Adventure Meet</li>
+            <li>English Language</li>
+            <li>Conservation and biodiversity</li>
+            <li>Rock climbing, ice climbing amd mountaineering (for expedition leaders)</li>
+        </ol>
+
+        <h2 class="text-success">Altitude Information</h2>
+        <p>What is Altitude?</p>
+        <p>Altitude is known as elevation, and it is often used to refer to the height above sea level. Altitude is related
+            to air pressure. Trekkers and mountaineers can measure their altitude by measuring the air pressure around them.
+            As the altitude rises, air pressure drops. In other words, if the indicated altitude is high, the air pressure
+            becomes low. The human body reacts to high altitudes. Decreased air pressure means that less oxygen is available
+            for breathing. One normal effect of altitude is shortness of breath since the lungs have to work harder to
+            deliver oxygen to the bloodstream. It can take days and even weeks for a body to adjust to high altitude and low
+            air pressure. To prevent severe altitude sickness, mountaineers bring supplemental (extra) supplies of oxygen
+            and limit their time in the 'high elevation'.
+            Acclimatization We are experts at operating adventure trips at high-altitude destinations. Every trip we run has
+            a realistic acclimatization program. Our treks are designed to allow gradual height gain, spread over several
+            days. There is no way of predicting who will suffer from altitude but, for the vast majority of people, a slow
+            ascent to height will produce minimal effects. Acclimatizing takes time and there are no safe shortcuts. All
+            trips to altitudes over 3,000 meters carry medicines essential to the treatment of altitude illness, and our
+            leaders are trained to administer them and to know the symptoms</p>
+
+        <h2 class="text-success">Altitude Information</h2>
+        <p>What is Altitude?</p>
+        <p>Altitude is known as elevation, and it is often used to refer to the height above sea level. Altitude is related
+            to air pressure. Trekkers and mountaineers can measure their altitude by measuring the air pressure around them.
+            As the altitude rises, air pressure drops. In other words, if the indicated altitude is high, the air pressure
+            becomes low. The human body reacts to high altitudes. Decreased air pressure means that less oxygen is available
+            for breathing. One normal effect of altitude is shortness of breath since the lungs have to work harder to
+            deliver oxygen to the bloodstream. It can take days and even weeks for a body to adjust to high altitude and low
+            air pressure. To prevent severe altitude sickness, mountaineers bring supplemental (extra) supplies of oxygen
+            and limit their time in the 'high elevation'.
+            Acclimatization We are experts at operating adventure trips at high-altitude destinations. Every trip we run has
+            a realistic acclimatization program. Our treks are designed to allow gradual height gain, spread over several
+            days. There is no way of predicting who will suffer from altitude but, for the vast majority of people, a slow
+            ascent to height will produce minimal effects. Acclimatizing takes time and there are no safe shortcuts. All
+            trips to altitudes over 3,000 meters carry medicines essential to the treatment of altitude illness, and our
+            leaders are trained to administer them and to know the symptoms</p>
+
+        <h2 class="text-success">Altitude Information</h2>
+        <p>What is Altitude?</p>
+        <p>Altitude is known as elevation, and it is often used to refer to the height above sea level. Altitude is related
+            to air pressure. Trekkers and mountaineers can measure their altitude by measuring the air pressure around them.
+            As the altitude rises, air pressure drops. In other words, if the indicated altitude is high, the air pressure
+            becomes low. The human body reacts to high altitudes. Decreased air pressure means that less oxygen is available
+            for breathing. One normal effect of altitude is shortness of breath since the lungs have to work harder to
+            deliver oxygen to the bloodstream. It can take days and even weeks for a body to adjust to high altitude and low
+            air pressure. To prevent severe altitude sickness, mountaineers bring supplemental (extra) supplies of oxygen
+            and limit their time in the 'high elevation'.
+            Acclimatization We are experts at operating adventure trips at high-altitude destinations. Every trip we run has
+            a realistic acclimatization program. Our treks are designed to allow gradual height gain, spread over several
+            days. There is no way of predicting who will suffer from altitude but, for the vast majority of people, a slow
+            ascent to height will produce minimal effects. Acclimatizing takes time and there are no safe shortcuts. All
+            trips to altitudes over 3,000 meters carry medicines essential to the treatment of altitude illness, and our
+            leaders are trained to administer them and to know the symptoms</p>
+    </section>
+
+    @include('inc.book-a-call-cta')
 @endsection
