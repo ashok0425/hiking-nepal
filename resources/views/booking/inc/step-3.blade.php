ac702a1a8123ab358d6965cdacd938ea3bbb6ca2
Priyash Patil <38959321+priyashpatil@users.noreply.github.com>
2024-12-06 17:07:07 +0530

    make step 3 responsive

--- resources/views/booking/inc/step-3.blade.php
+++ resources/views/booking/inc/step-3.blade.php
@@ -1,7 +1,7 @@
-<div class="brand-shadow p-5 mb-5 step-3">
-    <h2 class="mb-5 text-center">Choose your destination * <span class="text-danger">*</span></h2>
+<div class="brand-shadow p-3 p-md-5 mb-5 step-3">
+    <h2 class="mb-4 mb-md-5 text-center">Choose your destination * <span class="text-danger">*</span></h2>
 
-    <div class="row row-cols-4 g-3 mb-5">
+    <div class="row row-cols-2 row-cols-sm-3 row-cols-md-4 g-3 mb-4 mb-md-5">
         <div class="col">
             <div class="form-check">
                 <input class="form-check-input" type="checkbox" id="peru" name="destination[]" value="Peru">
@@ -58,10 +58,11 @@
         </div>
     </div>
 
-    <div class="d-flex  flex-wrap gap-3 justify-content-between align-items-center">
-        <div class="fw-bold">Choose the trips you are interested in</div>
-        <form action="" method="GET" class="mb-5">
-            <div class="input-group mb-3">
+    <div
+        class="d-flex flex-column flex-md-row gap-3 justify-content-between align-items-start align-items-md-center mb-4">
+        <div class="fw-bold w-100">Choose the trips you are interested in</div>
+        <form action="" method="GET" class="mb-3 mb-md-5  w-md-auto">
+            <div class="input-group" style="max-width: 300px;">
                 <span class="input-group-text text-primary" id="basic-addon1">
                     <i class="fas fa-search"></i>
                 </span>
@@ -70,60 +71,54 @@
         </form>
     </div>
 
-
-    <div class="row row-cols-4 g-3 mb-3">
-        <!-- Initial visible items -->
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3 mb-3">
         <div class="col">
             <div class="form-check">
                 <input class="form-check-input" type="checkbox" id="annapurna" name="trek[]" value="Annapurna">
-                <label class="form-check-label" for="annapurna">Annapurna base camp trek</label>
+                <label class="form-check-label text-break" for="annapurna">Annapurna base camp trek</label>
             </div>
         </div>
         <div class="col">
             <div class="form-check">
                 <input class="form-check-input" type="checkbox" id="bhutan_nepal" name="trek[]"
                     value="Bhutan_Nepal">
-                <label class="form-check-label" for="bhutan_nepal">Bhutan and Nepal Trek</label>
+                <label class="form-check-label text-break" for="bhutan_nepal">Bhutan and Nepal Trek</label>
             </div>
         </div>
         <div class="col">
             <div class="form-check">
                 <input class="form-check-input" type="checkbox" id="dolpo" name="trek[]" value="Dolpo">
-                <label class="form-check-label" for="dolpo">Lower Dolpo Trek</label>
+                <label class="form-check-label text-break" for="dolpo">Lower Dolpo Trek</label>
             </div>
         </div>
         <div class="col">
             <div class="form-check">
                 <input class="form-check-input" type="checkbox" id="bunjee" name="trek[]" value="Bunjee">
-                <label class="form-check-label" for="bunjee">Pokhara Bunjee Jumping</label>
+                <label class="form-check-label text-break" for="bunjee">Pokhara Bunjee Jumping</label>
             </div>
         </div>
-
-
     </div>
 
-    <!-- Collapsible content -->
     <div class="collapse" id="moreTreks">
-        <div class="row row-cols-4 g-3">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 g-3">
             <div class="col">
                 <div class="form-check">
                     <input class="form-check-input" type="checkbox" id="nepal_tibet" name="trek[]"
                         value="Nepal_Tibet">
-                    <label class="form-check-label" for="nepal_tibet">Nepal and Tibet Tour</label>
+                    <label class="form-check-label text-break" for="nepal_tibet">Nepal and Tibet Tour</label>
                 </div>
             </div>
             <div class="col">
                 <div class="form-check">
                     <input class="form-check-input" type="checkbox" id="upper_mustang" name="trek[]"
                         value="Upper_Mustang">
-                    <label class="form-check-label" for="upper_mustang">Upper Mustang Trek</label>
+                    <label class="form-check-label text-break" for="upper_mustang">Upper Mustang Trek</label>
                 </div>
             </div>
         </div>
     </div>
 
-    <!-- Show more/less button -->
-    <div class="text-center mb-5">
+    <div class="text-center mb-4 mb-md-5">
         <button class="btn btn-link" type="button" data-bs-toggle="collapse" data-bs-target="#moreTreks"
             aria-expanded="false" aria-controls="moreTreks">
             <span class="show-more">Show More <i class="fas fa-chevron-down"></i></span>
@@ -131,7 +126,6 @@
         </button>
     </div>
 
-    <!-- Add this JavaScript to handle show more/less text toggle -->
     <script>
         document.querySelector('[data-bs-toggle="collapse"]').addEventListener('click', function() {
             const showMore = this.querySelector('.show-more');
