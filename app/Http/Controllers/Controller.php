19c62d005388a77a6e30a0eb566417a2a794590f
Priyash Patil <38959321+priyashpatil@users.noreply.github.com>
2024-12-13 18:59:13 +0530

    chore: fix file storage

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,22 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Illuminate\Support\Facades\Storage;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+
+    public function uploadFile($path, $file)
+    {
+        $extension = $file->getClientOriginalExtension();
+        $filename = uniqid() . time() . '.' . $extension;
+        $dd = $file->storeAs('public/upload/', $filename, 'public');
+        return 'upload/' . $filename;
+    }
+
+    public function deleteFile($file)
+    {
+        Storage::disk('public')->delete($file);
+    }
 }
