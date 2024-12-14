cb575327905b18297e14262b508a4cd649d57f04
Priyash Patil <38959321+priyashpatil@users.noreply.github.com>
2024-12-14 13:26:19 +0530

    chore: add package_featured migration

---
+++ database/migrations/2024_12_14_075151_create_package_featured_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('package_featured', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('package_id')->references('id')->on('packages');
+            $table->unsignedInteger('featured_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('package_features');
+    }
+};
