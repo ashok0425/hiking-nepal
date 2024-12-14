401661c3c9f97acc15fd5c045d3579d7b57402e2
Priyash <38959321+priyashpatil@users.noreply.github.com>
2024-12-14 14:10:20 +0530

    chore: fix country migration

--- database/migrations/2023_03_19_015619_create_countries_table.php
+++ database/migrations/2023_03_19_015619_create_countries_table.php
@@ -13,8 +13,10 @@ class CreateCountriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('countries', function (Blueprint $table) {
+        Schema::create("countries", function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->string("slug")->unique();
             $table->timestamps();
         });
     }
@@ -26,6 +28,6 @@ class CreateCountriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('countries');
+        Schema::dropIfExists("countries");
     }
 }
