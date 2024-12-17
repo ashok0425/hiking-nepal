dccb589f61aaf9e7f785ad4bbd02c55fff7d8582
Priyash <38959321+priyashpatil@users.noreply.github.com>
2024-12-17 15:14:45 +0530

    chore: cleanup sidebar

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -13,104 +13,33 @@
               <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
                   data-accordion="false">
 
-                  {{-- dashboard section --}}
                   <li class="nav-item ">
-                      <a href="{{ route('admin.dashboard') }}" class="nav-link ">
+                      <a href="{{ route('admin.dashboard') }}"
+                          class="nav-link {{ Route::is('admin.dashboard') ? 'active' : '' }}">
                           <i class="nav-icon fas fa-tachometer-alt"></i>
-                          <p>
-                              Dashboard
-
-                          </p>
+                          <p>Dashboard</p>
                       </a>
-
                   </li>
 
-                  {{-- Destination  --}}
-                  <li class="nav-item <?php echo Request::segment(2) == 'destinations' ? 'menu-open' : ''; ?>">
-                      <a href="#" class="nav-link <?php echo Request::segment(2) == 'destinations' ? 'active' : ''; ?> ">
+                  <li class="nav-item">
+                      <a href="{{ route('admin.destinations.index') }}"
+                          class="nav-link {{ Route::is('admin.destinations.*') ? 'active' : '' }}">
                           <i class="nav-icon fas fa-map"></i>
-                          <p>
-                              Destinations
-                              <i class="fas fa-angle-down right"></i>
-
-                          </p>
+                          <p>Destinations</p>
                       </a>
-                      <ul class="nav nav-treeview">
-                          <li class="nav-item">
-                              <a href="{{ route('admin.destinations.index') }}" class="nav-link <?php echo Request::segment(3) == '' ? 'active' : ''; ?>">
-                                  <i class="far fa-circle nav-icon"></i>
-                                  <p>All </p>
-                              </a>
-                          </li>
-                          <li class="nav-item ">
-                              <a href="{{ route('admin.destinations.create') }}" class="nav-link <?php echo Request::segment(3) == 'create' ? 'active' : ''; ?>">
-                                  <i class="far fa-circle nav-icon"></i>
-                                  <p>Add Destination </p>
-                              </a>
-                          </li>
-
-                      </ul>
                   </li>
 
-                  <li class="nav-item <?php echo Request::segment(2) == 'places' ? 'menu-open' : ''; ?>">
-                      <a href="#" class="nav-link <?php echo Request::segment(2) == 'places' ? 'active' : ''; ?> ">
+                  <li class="nav-item">
+                      <a href="{{ route('admin.places.index') }}"
+                          class="nav-link {{ Route::is('admin.places.*') ? 'active' : '' }}">
                           <i class="nav-icon fas fa-mountain"></i>
-                          <p>
-                              Places
-                              <i class="fas fa-angle-down right"></i>
-
-                          </p>
+                          <p>Places</p>
                       </a>
-                      <ul class="nav nav-treeview">
-                          <li class="nav-item">
-                              <a href="{{ route('admin.places.index') }}" class="nav-link <?php echo Request::segment(3) == '' ? 'active' : ''; ?>">
-                                  <i class="far fa-circle nav-icon"></i>
-                                  <p>View All</p>
-                              </a>
-                          </li>
-                          <li class="nav-item ">
-                              <a href="{{ route('admin.places.create') }}" class="nav-link <?php echo Request::segment(3) == 'create' ? 'active' : ''; ?>">
-                                  <i class="far fa-circle nav-icon"></i>
-                                  <p>Add Category Place</p>
-                              </a>
-                          </li>
-
-                      </ul>
-                  </li>
-
-                  {{-- Category Destination  --}}
-                  <li class="nav-item <?php echo Request::segment(2) == 'categories-destinations' ? 'menu-open' : ''; ?>">
-                      <a href="#" class="nav-link <?php echo Request::segment(2) == 'categories-destinations' ? 'active' : ''; ?> ">
-                          {{-- <i class="nav-icon fas fa-skiing"></i> --}}
-                          <i class="nav-icon fas fa-hiking"></i>
-                          <p>
-                              Tour Categories
-                              <i class="fas fa-angle-down right"></i>
-
-                          </p>
-                      </a>
-                      <ul class="nav nav-treeview">
-                          <li class="nav-item">
-                              <a href="{{ route('admin.categories-destinations.index') }}"
-                                  class="nav-link <?php echo Request::segment(3) == '' ? 'active' : ''; ?>">
-                                  <i class="far fa-circle nav-icon"></i>
-                                  <p>Category</p>
-                              </a>
-                          </li>
-                          <li class="nav-item ">
-                              <a href="{{ route('admin.categories-destinations.create') }}"
-                                  class="nav-link <?php echo Request::segment(3) == 'create' ? 'active' : ''; ?>">
-                                  <i class="far fa-circle nav-icon"></i>
-                                  <p>Add Category </p>
-                              </a>
-                          </li>
-
-                      </ul>
                   </li>
 
                   <li class="nav-item <?php echo Request::segment(2) == 'categories-packages' ? 'menu-open' : ''; ?>">
                       <a href="#" class="nav-link <?php echo Request::segment(2) == 'categories-packages' ? 'active' : ''; ?> ">
-                          <i class="nav-icon fas fa-box"></i>
+                          <i class="nav-icon fas fa-route"></i>
                           <p>
                               Tours
                               <i class="fas fa-angle-down right"></i>
@@ -121,138 +50,45 @@
                           <li class="nav-item">
                               <a href="{{ route('admin.categories-packages.index') }}"
                                   class="nav-link <?php echo Request::segment(3) == '' ? 'active' : ''; ?>">
-                                  <i class="far fa-circle nav-icon"></i>
-                                  <p>View Packages</p>
-                              </a>
-                          </li>
-                          <li class="nav-item ">
-                              <a href="{{ route('admin.categories-packages.create') }}"
-                                  class="nav-link <?php echo Request::segment(3) == 'create' ? 'active' : ''; ?>">
-                                  <i class="far fa-circle nav-icon"></i>
-                                  <p>Add Packages </p>
+                                  {{-- <i class="fas fa-list nav-icon"></i> --}}
+                                  <p>All Tours</p>
                               </a>
                           </li>
 
-                          {{-- Category Package  --}}
+                          {{-- Tour Category  --}}
                           <li class="nav-item">
                               <a href="#" class="nav-link">
-                                  <i class="nav-icon fas fa-tag"></i>
-                                  <p>
-                                      Categories
-
-                                  </p>
+                                  {{-- <i class="nav-icon fas fa-hiking"></i> --}}
+                                  <p>Categories</p>
                               </a>
                           </li>
-
                       </ul>
                   </li>
 
                   {{-- Departure Date  --}}
-                  <li class="nav-item <?php echo Request::segment(2) == 'departures' ? 'menu-open' : ''; ?>">
-                      <a href="#" class="nav-link <?php echo Request::segment(2) == 'departures' ? 'active' : ''; ?> ">
+                  {{-- <li class="nav-item">
+                      <a href="{{ route('admin.departures.index') }}"
+                          class="nav-link {{ Request::segment(2) == 'departures' ? 'active' : '' }} ">
                           <i class="nav-icon fas fa-plane-departure"></i>
-                          <p>
-                              Departure Dates
-                              <i class="fas fa-angle-down right"></i>
-
-                          </p>
+                          <p>Departure Dates</p>
                       </a>
-                      <ul class="nav nav-treeview">
-                          <li class="nav-item">
-                              <a href="{{ route('admin.departures.index') }}" class="nav-link <?php echo Request::segment(3) == '' ? 'active' : ''; ?>">
-                                  <i class="far fa-circle nav-icon"></i>
-                                  <p>View All</p>
-                              </a>
-                          </li>
-                          <li class="nav-item ">
-                              <a href="{{ route('admin.departures.create') }}" class="nav-link <?php echo Request::segment(3) == 'create' ? 'active' : ''; ?>">
-                                  <i class="far fa-circle nav-icon"></i>
-                                  <p>Add New </p>
-                              </a>
-                          </li>
+                  </li> --}}
 
-                      </ul>
-                  </li>
-
-                  {{-- Testimonial  --}}
-                  <li class="nav-item <?php echo Request::segment(2) == 'testimonials' ? 'menu-open' : ''; ?>">
-                      <a href="#" class="nav-link <?php echo Request::segment(2) == 'testimonials' ? 'active' : ''; ?> ">
+                  <li class="nav-item">
+                      <a href="{{ route('admin.testimonials.index') }}"
+                          class="nav-link {{ Request::segment(2) == 'testimonials' ? 'active' : '' }} ">
                           <i class="nav-icon fas fa-star"></i>
-                          <p>
-                              Testimonials
-                              <i class="fas fa-angle-down right"></i>
-
-                          </p>
+                          <p>Reviews</p>
                       </a>
-                      <ul class="nav nav-treeview">
-                          <li class="nav-item">
-                              <a href="{{ route('admin.testimonials.index') }}" class="nav-link <?php echo Request::segment(3) == '' ? 'active' : ''; ?>">
-                                  <i class="far fa-circle nav-icon"></i>
-                                  <p>View Testimonials</p>
-                              </a>
-                          </li>
-                          <li class="nav-item ">
-                              <a href="{{ route('admin.testimonials.create') }}"
-                                  class="nav-link <?php echo Request::segment(3) == 'create' ? 'active' : ''; ?>">
-                                  <i class="far fa-circle nav-icon"></i>
-                                  <p>Add Testimonial </p>
-                              </a>
-                          </li>
-
-                      </ul>
                   </li>
 
-                  {{-- Faq  --}}
-                  <li class="nav-item <?php echo Request::segment(2) == 'faqs' ? 'menu-open' : ''; ?>">
-                      <a href="#" class="nav-link <?php echo Request::segment(2) == 'faqs' ? 'active' : ''; ?> ">
-                          <i class="nav-icon fas fa-question-circle"></i>
-                          <p>
-                              FAQs
-                              <i class="fas fa-angle-down right"></i>
-
-                          </p>
-                      </a>
-                      <ul class="nav nav-treeview">
-                          <li class="nav-item">
-                              <a href="{{ route('admin.faqs.index') }}" class="nav-link <?php echo Request::segment(3) == '' ? 'active' : ''; ?>">
-                                  <i class="far fa-circle nav-icon"></i>
-                                  <p>View All</p>
-                              </a>
-                          </li>
-                          <li class="nav-item ">
-                              <a href="{{ route('admin.faqs.create') }}" class="nav-link <?php echo Request::segment(3) == 'create' ? 'active' : ''; ?>">
-                                  <i class="far fa-circle nav-icon"></i>
-                                  <p>Add New </p>
-                              </a>
-                          </li>
-
-                      </ul>
-                  </li>
-
-                  {{-- Posts --}}
-                  <li class="nav-item <?php echo Request::segment(2) == 'posts' ? 'menu-open' : ''; ?>">
-                      <a href="#" class="nav-link <?php echo Request::segment(2) == 'posts' ? 'active' : ''; ?> ">
-                          {{-- <i class="nav-icon fas fa-newspaper"></i> --}}
+                  {{-- Blog --}}
+                  <li class="nav-item">
+                      <a href="{{ route('admin.posts.index') }}"
+                          class="nav-link {{ Route::is('admin.posts.*') ? 'active' : '' }}">
                           <i class="nav-icon fas fa-quote-left"></i>
-                          <p>
-                              Blog
-                              <i class="fas fa-angle-down right"></i>
-                          </p>
+                          <p>Blog</p>
                       </a>
-                      <ul class="nav nav-treeview">
-                          <li class="nav-item">
-                              <a href="{{ route('admin.posts.index') }}" class="nav-link <?php echo Request::segment(3) == '' ? 'active' : ''; ?>">
-                                  <i class="far fa-circle nav-icon"></i>
-                                  <p>View All</p>
-                              </a>
-                          </li>
-                          <li class="nav-item ">
-                              <a href="{{ route('admin.posts.create') }}" class="nav-link <?php echo Request::segment(3) == 'create' ? 'active' : ''; ?>">
-                                  <i class="far fa-circle nav-icon"></i>
-                                  <p>Add New</p>
-                              </a>
-                          </li>
-                      </ul>
                   </li>
 
                   {{-- Newsletter  --}}
@@ -269,15 +105,14 @@
 
                           <li class="nav-item ">
                               <a href="{{ route('admin.newsletters.index') }}" class="nav-link <?php echo Request::segment(3) == '' ? 'active' : ''; ?>">
-                                  <i class="far fa-circle nav-icon"></i>
-                                  <p>View All </p>
+                                  {{-- <i class="far fa-circle nav-icon"></i> --}}
+                                  <p>Subscribers</p>
                               </a>
                           </li>
 
                           <li class="nav-item ">
-                              <a href="{{ route('admin.newsletter.history') }}"
-                                  class="nav-link <?php echo Request::segment(3) == 'emailhistory' ? 'active' : ''; ?>">
-                                  <i class="far fa-circle nav-icon"></i>
+                              <a href="{{ route('admin.newsletter.history') }}" class="nav-link <?php echo Request::segment(3) == 'emailhistory' ? 'active' : ''; ?>">
+                                  {{-- <i class="far fa-circle nav-icon"></i> --}}
                                   <p>Email History </p>
                               </a>
                           </li>
@@ -286,7 +121,7 @@
                   </li>
 
                   {{-- Contact  --}}
-                  <li class="nav-item <?php echo Request::segment(2) == 'contacts' ? 'menu-open' : ''; ?>">
+                  {{-- <li class="nav-item <?php echo Request::segment(2) == 'contacts' ? 'menu-open' : ''; ?>">
                       <a href="#" class="nav-link <?php echo Request::segment(2) == 'contacts' ? 'active' : ''; ?> ">
                           <i class="nav-icon fas fa-users"></i>
                           <p>
@@ -305,7 +140,7 @@
                           </li>
 
                       </ul>
-                  </li>
+                  </li> --}}
               </ul>
           </nav>
           <!-- /.sidebar-menu -->
