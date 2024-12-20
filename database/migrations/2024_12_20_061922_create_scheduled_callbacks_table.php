598143dee045e57104471431443b5025ad0f7726
Priyash <38959321+priyashpatil@users.noreply.github.com>
2024-12-20 11:54:44 +0530

    chore: add scheduled callback model

---
+++ database/migrations/2024_12_20_061922_create_scheduled_callbacks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('scheduled_callbacks', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('email');
+            $table->text('comments');
+            $table->date('callback_date');
+            $table->string('callback_time');
+            $table->text('callback_message')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('scheduled_callbacks');
+    }
+};
