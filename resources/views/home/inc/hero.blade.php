13dff4aa7b8f4f8723668222dd3c0d7baea6b5ce
Priyash <38959321+priyashpatil@users.noreply.github.com>
2024-12-21 14:29:22 +0530

    chore: add home page search redirect to deals page

--- resources/views/home/inc/hero.blade.php
+++ resources/views/home/inc/hero.blade.php
@@ -3,12 +3,13 @@
     <div class="hero-cloud-l1 animate__animated animate__slideInLeft"></div>
 
     <h1 class="animate__animated animate__slideInUp text-white">Hiking Nepal</h1>
-    <form action="" method="GET" class="animate__animated animate__zoomIn">
+    <form action="{{ route('deals') }}" method="GET" class="animate__animated animate__zoomIn">
         <div class="input-group mb-3">
             <span class="input-group-text text-primary" id="basic-addon1">
                 <i class="fas fa-search"></i>
             </span>
-            <input class="form-control ps-0 border-start-0" type="text" placeholder="Find your trip..." required>
+            <input name="search" class="form-control ps-0 border-start-0" type="text"
+                placeholder="Find your trip..." required>
         </div>
     </form>
 
