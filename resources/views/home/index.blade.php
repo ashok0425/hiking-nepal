9f516a8a804201a44d30498c260c4f61c6a1b141
Priyash <38959321+priyashpatil@users.noreply.github.com>
2024-12-22 11:27:15 +0530

    chore: add homepage meta tags

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,6 +2,30 @@
 
 @section('title', 'Welcome')
 
+@section('meta')
+    {{-- Primary Meta Tags --}}
+    <meta name="description"
+        content="Discover the beauty of Nepal with our amazing hiking and trekking packages. Experience breathtaking mountain views, cultural heritage sites and unforgettable adventures.">
+    <meta name="keywords" content="hiking nepal, trekking nepal, nepal tours, mountain hiking, adventure travel nepal">
+
+    {{-- Open Graph / Facebook --}}
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="Hiking Nepal">
+    <meta property="og:title" content="Welcome - Hiking Nepal">
+    <meta property="og:description"
+        content="Discover the beauty of Nepal with our amazing hiking and trekking packages. Experience breathtaking mountain views, cultural heritage sites and unforgettable adventures.">
+    <meta property="og:image" content="https://hikingnepal.com/wp-content/uploads/2022/11/himalayas-3989411.jpg">
+    <meta property="og:url" content="{{ url()->current() }}">
+
+    {{-- Twitter --}}
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@hikingnepal">
+    <meta name="twitter:title" content="Welcome - Hiking Nepal">
+    <meta name="twitter:description"
+        content="Discover the beauty of Nepal with our amazing hiking and trekking packages. Experience breathtaking mountain views, cultural heritage sites and unforgettable adventures.">
+    <meta name="twitter:image" content="https://hikingnepal.com/wp-content/uploads/2022/11/himalayas-3989411.jpg">
+@endsection
+
 @section('content')
     @include('home.inc.hero')
     @include('home.inc.popular-destinations')
