1849bdb0ee2f413e66b017f47e7456acac1ce41f
Priyash <38959321+priyashpatil@users.noreply.github.com>
2024-12-26 14:59:19 +0530

    chore: update faq color

--- resources/views/inc/footer.blade.php
+++ resources/views/inc/footer.blade.php
@@ -9,7 +9,6 @@
                     <h6 class="text-success mb-3">Let’s Explore Places</h6>
                     <ul class="list-unstyled">
                         <li><a href="#">Keshar Mahal, Thamel, Kathmandu</a></li>
-                        <li><a href="mailto:info@hikingnepal.com">info@hikingnepal.com</a></li>
                         <li><a href="tel:+97714701537">+977 1 4701537</a></li>
                         <li><a href="tel:+9779802342080">+977 9802342080</a></li>
                         <li><a href="mailto:info@hikingnepal.com">info@hikingnepal.com</a></li>
