f3f8ae0d284174f6e51d7f56234d6018fd408019
Priyash <38959321+priyashpatil@users.noreply.github.com>
2024-12-30 19:12:53 +0530

    chore: add whatsapp popup

--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -39,6 +39,10 @@
         @yield('content')
     </main>
 
+    <div class="wa-popup"><a href="https://api.whatsapp.com/send?phone=9779802342080"
+            class="d-inline-flex align-items-center gap-1 text-dark"><img src="{{ asset('images/whatsapp-hd.png') }}"
+                alt="whatsapp" width="50" height="50"></a></div>
+
     @include('inc.footer')
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
