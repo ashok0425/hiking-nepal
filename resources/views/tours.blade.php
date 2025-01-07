9b9fc76bac254c070327a03ae24d6aadc22addca
Priyash <38959321+priyashpatil@users.noreply.github.com>
2025-01-07 15:04:49 +0530

    chore: change cta button color on tours page

--- resources/views/tours.blade.php
+++ resources/views/tours.blade.php
@@ -384,7 +384,7 @@
                         </tbody>
                     </table>
 
-                    <a class="btn bg-white text-cta w-100 blink-button" href="{{ route('book-trip') }}">BOOK NOW</a>
+                    <a class="btn bg-success text-white w-100 blink-button" href="{{ route('book-trip') }}">BOOK NOW</a>
                 </div>
 
                 <div class="position-sticky" style="top: 220px;">
